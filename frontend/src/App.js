5024f177c45ac209e0bfc4b119f51de51796fbe2
Ammar Nanjiani <ammar123456@gmail.com>
2021-10-09 13:57:26 -0500

    add code for Note to interact with API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,12 @@ import "tailwindcss/tailwind.css"
 
 function App() {
   const [noteData, setNoteData] = useState([])
+  const [noteTags, setNoteTags] = useState([])
 
   axios.defaults.baseURL = 'http://localhost:8000/brain2_api'
 
   useEffect(()=>{
-    axios.get('http://localhost:8000/brain2_api/note/')
+    axios.get('/note/')
       .then(res=>{
         console.log(res)
         console.log(res.data);
@@ -21,14 +22,22 @@ function App() {
       .catch(err=>{
         console.log(err);
       })
+    
+    axios.get('/note_tag/').then(response => {
+      setNoteTags(response.data)
+    })    
   }, []);
+
+  function updateNoteTags(val) {
+    setNoteTags([...noteTags, val])
+  }
 
 
   return (
     <>
       {
         noteData.map((data, id)=>{
-          return <Note data={data} />;
+          return <Note data={data} noteTags={noteTags} updateNoteTags={updateNoteTags} />;
         })
       }
     </>
--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,28 +1,49 @@
 import axios from 'axios';
 import React, {useState, useEffect, useRef, useCallback} from 'react';
 import ContentEditable from 'react-contenteditable';
-import ReactDOMServer from 'react-dom/server';
 import ReactTags from 'react-tag-autocomplete';
-import Autosuggest from 'react-autosuggest'
 import "./note-styles.css"
 
+function clean_string(myString) {
+  let strippedHtml = myString.replace(/<[^>]+>/g, '')
+
+  return strippedHtml
+}
+
 function Annotation(props) {
-  if(!props.annotation) {
-    return null
+  
+  const [annotation, setAnnotation] = useState(props.annotation)
+  const text = useRef("")
+
+  const handleClick = () =>{
+    setAnnotation(" ")
   }
 
-  const text = useRef(props.annotation)
+  if(!annotation || annotation == "") {
+
+    return <div class="p-5 flex justify-center">
+      <button onClick={handleClick} class="bg-gray-200 rounded-full px-2 py-2 text-base font-semibold text-gray-700 mr-2 mb-2">&#43; Add a note</button>
+    </div>
+  }
+
+
+  text.current = annotation
 
   const handleChange = evt => {
     text.current = evt.target.value
-    console.log(text.current)
   }
 
   const handleBlur = () => {
     if (text.current != props.annotation)
-      axios.patch('/note/' + props.id + '/', {annotation : text.current}).then(res => {
+      axios.patch('/note/' + props.id + '/', {annotation : clean_string(text.current)}).then(res => {
         console.log(res)
       });
+  }
+
+  const keyPress = evt => {
+    if(evt.keyCode == 13){
+      evt.target.blur()
+    }
   }
 
   return (
@@ -32,6 +53,7 @@ function Annotation(props) {
         html={props.annotation}
         onChange={handleChange}
         onBlur={handleBlur}
+        onKeyDown={keyPress}
       />
     </div>
     )
@@ -53,7 +75,7 @@ function LinkedNotes(props) {
 
 function Tag(props) {
 
-  return <span class="flex-1 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+  return <span class="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
       <button onClick={props.onDelete} class="text-gray-600">&times; &nbsp;</button>
       {props.tag.name}
   </span>
@@ -61,30 +83,56 @@ function Tag(props) {
 }
 
 function Tags(props) {
-  const [allTags, setAllTags] = useState([])
-  const [tags, setTags] = useState([])
+  
   // const reactTags = useRef()
 
-  useEffect(() => {
-    axios.get('/note_tag/').then(response => {
-      setAllTags(response.data)
-      let myTags = response.data.filter(function (tag) {
-        return props.tags.includes(tag.id)
-      })
-      setTags(myTags)
+  const [tags, setTags] = useState([])
+  
+
+  useEffect(() =>{
+    console.log(props.allTags)
+    let myTags = props.allTags.filter(function (tag) {
+      return props.tags.includes(tag.id)
     })
-  }, [])
+    console.log(myTags)
+
+    setTags(myTags)
+  })
+
 
   const onDelete = useCallback((tagIndex) => {
-    setTags(tags.filter((_, i) => i !== tagIndex))
+    let newTags = tags.filter((_, i) => i !== tagIndex)
+    setTags(newTags)
+    axios.patch('/note/' + props.note_id + '/', {tags : newTags.map(e => e.id)}).then(res => {
+      console.log(res)
+    });
+
   }, [tags])
 
   const onAddition = useCallback((newTag) => {
-    setTags([...tags, newTag])
-    console.log('adding')
-  }, [tags])
+    console.log(newTag)
+    const idxAllTags = props.allTags.map(e => e.name).indexOf(newTag.name)
+    console.log(idxAllTags)
+    let newTags = []
 
-  console.log(allTags)
+    if(idxAllTags === -1) {
+      axios.post('/note_tag/', {name: newTag.name}).then(response => {
+        console.log(response)
+        newTags = [...tags, response.data]
+        props.updateAllTags(response.data)
+        setTags(newTags)
+        axios.patch('/note/' + props.note_id + '/', {tags : newTags.map(e => e.id)}).then(res => {
+          console.log(res)
+        });
+      })
+    } else {
+      newTags = [...tags, props.allTags[idxAllTags]]
+      setTags(newTags)
+      axios.patch('/note/' + props.note_id + '/', {tags : newTags.map(e => e.id)}).then(res => {
+        console.log(res)
+      });
+    }
+  }, [tags])
 
   const reactTagsClassNames = {
     root: 'react-tags',
@@ -106,14 +154,13 @@ function Tags(props) {
       <div class="px-6 pt-4 pb-2">
         <ReactTags 
           tags={tags}
-          suggestions={allTags}
+          suggestions={props.allTags}
           onDelete={onDelete}
           onAddition={onAddition}
           tagComponent={Tag}
           allowBackspace={false}
           minQueryLength={1}
           allowNew
-          newTagText
           addOnBlur={true}
           placeholderText="Add a tag"
           classNames={reactTagsClassNames}
@@ -137,9 +184,10 @@ function Note(props) {
     const date = new Date(props.data.datetime);
 
     const [linked, setLinked] = useState([])
+    const highlight = useRef(props.data.highlight)
+    const chapter = useRef(props.data.chapter)
 
-    const [tags, setTags] = useState([])
-    
+    // TODO: Add LinkedNotes functionality
     // useEffect(() => {
     //   // let linked_notes = []
     //   // let linked_notes_promises = []
@@ -151,19 +199,39 @@ function Note(props) {
 
     //   // Promise.all(linked_notes_promises).then(() => setLinked(linked_notes))
 
-    //   let tags_promises = []
-    //   let tags = []
-
-    //   for (let i = 0; i < props.data.tags.length; i++) {
-    //     tags_promises.push(axios.get('/note_tag/' + props.data.tags[i]).then(response => {
-    //       tags.push(response)
-    //     }))
-    //   }
-
-    //   Promise.all(tags_promises).then(() => setTags(tags))
-
     //   // axios.get('/book/' + props.data.book).then(() => setTags)
     // }, [])
+
+    const handleChange = evt => {
+      highlight.current = evt.target.value
+    }
+  
+    const handleBlur = () => {
+      if (highlight.current != props.data.highlight)
+        axios.patch('/note/' + props.data.id + '/', {highlight : clean_string(highlight.current)}).then(res => {
+          console.log(res)
+        });
+    }
+
+    const keyPress = evt => {
+      if(evt.keyCode == 13){
+        evt.target.blur()
+      }
+    }
+
+    const chHandleChange = evt => {
+      chapter.current = evt.target.value
+    }
+  
+    const chHandleBlur = () => {
+      if (chapter.current != props.data.chapter)
+        axios.patch('/note/' + props.data.id + '/', {chapter : clean_string(chapter.current)}).then(res => {
+          console.log(res)
+        });
+    }
+
+    console.log('props from Note component')
+    console.log(props.data.tags)
     
     return (
       <>
@@ -171,17 +239,29 @@ function Note(props) {
         <div class="w-full rounded overflow-hidden shadow-lg">
           <div class="divide-y">
             <div class="flow-root text-lg text-black px-10 py-4">
-              <span class="float-left">{props.data.chapter}</span>
+              <span class="float-left">
+                <ContentEditable 
+                html={props.data.chapter}
+                onChange={chHandleChange}
+                onBlur={chHandleBlur}
+                onKeyDown={keyPress}               
+                />
+              </span>
               <span class="float-right">{date.toLocaleDateString()}</span>
             </div>
             <div class="p-10">
               <p class="text-black text-base">
-                {props.data.highlight}
+                <ContentEditable 
+                  html={props.data.highlight}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  onKeyDown={keyPress}    
+                />
               </p>
             </div>
             <Annotation annotation={props.data.annotation} id={props.data.id} />
             <LinkedNotes notes={linked} />
-            <Tags tags={props.data.tags} />
+            <Tags note_id={props.data.id} tags={props.data.tags} allTags={props.noteTags} updateAllTags={props.updateNoteTags} />
           </div>
         </div>
       </div>

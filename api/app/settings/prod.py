dcf0a70d241d2349ba2342d0a10c7df4d649cb7e
Ammar Nanjiani <ammar123456@gmail.com>
2021-10-11 18:27:16 -0500

    configure postgres for prod

--- api/app/settings/prod.py
+++ api/app/settings/prod.py
@@ -1,5 +1,6 @@
 import os
 from app.settings.base import *
+import dj_database_url
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
@@ -18,3 +19,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
 WHITENOISE_ROOT = os.path.join(BASE_DIR, "../..", "frontend", "build", "root")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
